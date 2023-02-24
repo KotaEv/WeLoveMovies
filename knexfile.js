c20fff100a6250bf72ccbe6b8006ddaa7434eac8
Dakota Everston <dakota.everston@gmail.com>
2023-02-23 20:25:17 -0700

    test

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ const path = require("path");
 require("dotenv").config();
 
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL = "postgres://bfqzzvcm:5MLtcI-BCHTJhcP7dJyJwPat_7OEUNHi@suleiman.db.elephantsql.com/bfqzzvcm",
 } = process.env;
 module.exports = {
   development: {
