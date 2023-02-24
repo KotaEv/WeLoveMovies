756a523c66e84ae2a4de18922f1e891872f84e6c
Dakota Everston <dakota.everston@gmail.com>
2023-02-23 20:32:36 -0700

    updated knex file with hardcoded env

--- knexfile.js
+++ knexfile.js
@@ -3,8 +3,9 @@ const path = require("path");
 require("dotenv").config();
 
 const {
-  DATABASE_URL = "postgres://bfqzzvcm:5MLtcI-BCHTJhcP7dJyJwPat_7OEUNHi@suleiman.db.elephantsql.com/bfqzzvcm",
+  DATABASE_URL = "postgres://bfqzzvcm:5MLtcI-BCHTJhcP7dJyJwPat_7OEUNHi@suleiman.db.elephantsql.com/bfqzzvcm"
 } = process.env;
+
 module.exports = {
   development: {
     client: "postgresql",
