26fba626f5e7bc47f2bb63b8fcc323849014d2ed
Dakota Everston <dakota.everston@gmail.com>
2023-02-23 18:27:07 -0700

    updated gitignore per instructions and changed knexfile

--- knexfile.js
+++ knexfile.js
@@ -3,9 +3,8 @@ const path = require("path");
 require("dotenv").config();
 
 const {
-  DATABASE_URL
+  DATABASE_URL = "postgresql://postgres@localhost/postgres",
 } = process.env;
-
 module.exports = {
   development: {
     client: "postgresql",
